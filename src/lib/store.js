4d77e495088fcad986ae89b211879da4f275ef3d
Hyeonjin Jeong <bangawork7@gmail.com>
2022-02-16 23:17:07 +0900

    메인페이지 구조 잡기 완료

--- src/lib/store.js
+++ src/lib/store.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from '../features/user/UserSlice';
 import auctionSlice from '../features/auction/AuctionSlice';
+import mainSlice from '../features/main/MainSlice';
 
 const reducer = combineReducers({
+  main: mainSlice.reducer,
   user: userSlice.reducer,
   auction: auctionSlice.reducer,
 });
