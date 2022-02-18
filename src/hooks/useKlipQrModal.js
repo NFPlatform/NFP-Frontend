a49bb1424dfb420accdafc3e862098144d10c321
Hyeonjin Jeong <bangawork7@gmail.com>
2022-02-18 22:03:21 +0900

    KlipQrComponent 디자인 수정

--- src/hooks/useKlipQrModal.js
+++ src/hooks/useKlipQrModal.js
@@ -1,10 +1,25 @@
 import { Box, Modal } from '@mui/material';
 import QRCode from 'qrcode.react';
 import { useState } from 'react';
+import Typography from '@mui/material/Typography';
+import { CloseRounded } from '@mui/icons-material';
 
 const useKlipQrModal = () => {
   const [klipQrModal, setKlipQrModal] = useState(false);
   const [klipQrUrl, setKlipQrUrl] = useState('');
+
+  const actionWithRedirectUrl = (redirectUrl) => {
+    if (redirectUrl.startsWith('kakaotalk')) {
+      history.push(redirectUrl);
+    } else {
+      setKlipQrUrl(redirectUrl);
+      setKlipQrModal(true);
+    }
+  };
+
+  const modalCloseAction = () => {
+    setKlipQrModal(false);
+  };
 
   const klipQrComponent = (
     <Modal
@@ -19,32 +34,42 @@ const useKlipQrModal = () => {
           top: '50%',
           left: '50%',
           transform: 'translate(-50%, -50%)',
-          width: 400,
+          width: 220,
+          height: 230,
           bgcolor: 'background.paper',
-          border: '2px solid #000000',
-          boxShadow: 2,
+          borderRadius: 5,
+          boxShadow: 3,
           p: 4,
           display: 'flex',
-          justifyContent: 'center',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          alignItems: 'center',
         }}
       >
-        <QRCode value={klipQrUrl} size={350} style={{ margin: 'auto' }} />
+        <CloseRounded
+          sx={{
+            position: 'absolute',
+            top: '5%',
+            right: '5%',
+            cursor: 'pointer',
+          }}
+          onClick={modalCloseAction}
+        />
+        <Typography
+          variant={'h6'}
+          component={'div'}
+          fontWeight={'medium'}
+          mb={2}
+        >
+          Klip 지갑 연동
+        </Typography>
+        <QRCode value={klipQrUrl} size={130} style={{ marginBottom: 10 }} />
+        <Typography variant={'subtitle2'} component={'div'}>
+          QR 코드를 스캔해주세요
+        </Typography>
       </Box>
     </Modal>
   );
-
-  const actionWithRedirectUrl = (redirectUrl) => {
-    if (redirectUrl.startsWith('kakaotalk')) {
-      history.push(redirectUrl);
-    } else {
-      setKlipQrUrl(redirectUrl);
-      setKlipQrModal(true);
-    }
-  };
-
-  const modalCloseAction = () => {
-    setKlipQrModal(false);
-  };
 
   return {
     klipQrComponent,
