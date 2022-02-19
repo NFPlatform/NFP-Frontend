8c4a1939cc51c86698ed31b6c1103c678eb1c3ec
Yeonhyuk <yhkim@autocrypt.io>
2022-02-19 22:19:51 +0900

    - 카카오 로그인 개발 / 운영 분리

--- src/lib/oauth.js
+++ src/lib/oauth.js
@@ -1,4 +1,7 @@
 const CLIENT_ID = '38dcf74ab38b0a33fbc9486eb332420e';
-const REDIRECT_URI = 'http://localhost:3000/oauth/';
+const REDIRECT_URI =
+  process.env.REACT_APP_ENV === 'production'
+    ? 'https://api.nfplatform.com/oauth/'
+    : 'http://localhost:3000/oauth/';
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&prompt=login`;
